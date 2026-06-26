f73bd159034db792c8db386fc8031a30a7482f8f
Artem <weter882@gmail.com>
2026-06-26 12:51:36 +0300

    add paymentService

---
+++ paymentService/src/test/java/com/artem/paymentservice/TestPaymentServiceApplication.java
@@ -0,0 +1,11 @@
+package com.artem.paymentservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(PaymentServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
